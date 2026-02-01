0a843261814ab9e8518c565feb67ab19a40e95d0
Shiva Verma <shivaverma30july@gmail.com>
2026-02-01 19:54:22 +0530

    [shiva verma]:created userdto class

---
+++ src/main/java/com/gevernova/entity/UserDTO.java
@@ -0,0 +1,26 @@
+package com.gevernova.entity;
+
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+
+    public UserDTO() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
